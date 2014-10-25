343e346eb7ca80da6347f663db7cba4dda56bdbb
DibPuelma <edib@grapeleaf.cl>
2014-10-24 23:05:30 -0300

    Realizada la base para los datos del gobierno, falta que el request funcione

--- lib/parser/gov_data_source_adapter.rb
+++ lib/parser/gov_data_source_adapter.rb
@@ -1,4 +1,38 @@
-class GovDataSourceAdapter < HtmlSourceAdapter
+class GovDataSourceAdapter < JSONSourceAdapter
 @searchURI
 @params
+	def getAPIJSON() #FALTA IMPLEMENTAR REQUEST, ESTA FALLA
+		uri = URI.parse("http://api.recursos.datos.gob.cl/datastreams/search?query=@query&auth_key=05579a63c4aea8106f4e2f19726255c45ddf689a")
+		http = Net::HTTP.new(uri.host, uri.port)
+		http.use_ssl = true
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE # read into this
+		@data = http.get(uri.request_uri)
+		return @data
+	end
+
+	def buildJSONAPI(parsedData, limit)
+		ret = []
+		parsedData["description"].each do |desc|
+			@contents << desc
+		end
+		parsedData["title"].each do |titl|
+			@titles << titl
+		end
+		parsedData["link"].each do |l|
+			@urls << l
+		end
+		parsedData["created_at"].each do |cat|
+			@dates << cat
+		end
+		parsedData["tags"].each do |t|
+			@tags << t
+		end
+		i = 0
+		type = gobierno_de_chile
+		while (i < limit && i < contents.length)
+			json={'source' => nil, 'title'=>  titles[i], 'content' => contents[i],'date' => dates[i],'source'=> {'url'=> urls[i], 'type' => type, 'extras' => tags[i]}}.to_json
+			ret << json
+		end
+		return ret
+	end
 end
--- lib/parser/json_source_adapter.rb
+++ lib/parser/json_source_adapter.rb
@@ -1,13 +1,29 @@
 class JSONSourceAdapter < SourceAdapter
-	def getJSON(limit)
 
+	def initialize(query_params,url)
+    	#String que contiene las palabras ingresadas por el usuario
+    	super(query_params)
+    	@url=url
+    	@offset=0
+  	end
+
+	def getJSON(limit)
+		ret = []
+		if limit <0
+			raise "Limit debe ser mayor a 0"
+		end
+		result = getAPIJSON()
+		puts result
+		parsedData = JSON.parse(result)
+		ret = buildJSONAPI(parsedData, limit)
+		return ret[0,limit] #Con esto igual se devuelve el límite, conversar eficiencia sobre el while
 	end
 
 	def getAPIJSON
 		
 	end
 	
-	def buildJSONAPI(JSON)
-		
+	def buildJSONAPI(parsedData, limit)
+	
 	end
 end

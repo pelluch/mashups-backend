519d5d90210781ebebd457a9b0715647573af8c2
lukas <lukas.zorich@gmail.com>
2014-10-28 00:13:46 -0300

    Adaptador cnn parcialmente implementado

--- lib/parser/cnn_source_adapter.rb
+++ lib/parser/cnn_source_adapter.rb
@@ -1,4 +1,25 @@
 class CNNSourceAdapter < HtmlSourceAdapter
 @searchURI
 @params
+
+	def initialize(query_params)
+		query=query_params
+		super(query_params, create_url(query_params, 0))
+	end
+
+	def buildJsonHtml(nokogiri_html)
+	ret = []
+	nokogiri_html.css(".
+	end
+
+	def nextHtml(current_nokogiri_html)
+
+	end
+
+	def create_url(query_params=nil, offset=0)
+		query_params=query_params==nil ? self.query_params : query_params
+
+		return URI::encode("http://edition.cnn.com/search/?query=#{query_params}&x=0&y=0&primaryType=mixed&sortBy=relevance&intl=true#&sortBy=date")
+	end
+
 end

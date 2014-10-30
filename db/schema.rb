d1bb12a9ced91f1f38a42fa9519acb7267fc6619
Tomster <tomster@emberjs.com>
2014-10-30 20:17:02 -0300

    Add spec files

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141028190558) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "keywords", force: true do |t|
     t.string   "keyword"
     t.integer  "value"
---
+++ spec/lib/parser_ai_facade/query_manager_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "QueryManager" do
+
+    describe :parse_and_filter do
+        
+
+    end
+
+end
